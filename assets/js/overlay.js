850ee7e789a20bb4fcd1f19bbe8ac0c5c71de43d
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-12-21 00:12:12 +0100

    orientamento vid aggiunto

---
+++ assets/js/overlay.js
@@ -0,0 +1,9 @@
+function overlayOn(){
+    document.getElementById("overlayMain").style.display = "block";
+}
+
+function overlayOff(){
+    document.getElementById("overlayMain").style.display = "none";
+}
+
+overlayOff();
