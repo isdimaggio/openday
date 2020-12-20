f229c049f3b2118e36b491ee7737ddd946adbe01
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-12-20 20:43:09 +0100

    marzipano fix 2 s.pov + descrizioni

--- tour/data.js
+++ tour/data.js
@@ -40,8 +40,8 @@ var APP_DATA = {
         {
           "yaw": -0.4039984216314938,
           "pitch": -0.0473958141708799,
-          "title": "Title",
-          "text": "Text"
+          "title": "Esterno",
+          "text": "L’Istituto Tecnico “L. Di Maggio”, nato dall’aggregazione del “Tecnico” ITIS Di Maggio e della “Ragioneria” ITC Amaduzzi, è attualmente composto da due settori: Tecnologico (Biotecnologie Sanitarie - Elettronica ed Elettrotecnica - Informatica e Telecomunicazioni) ed Economico (Amministrazione, Finanza e Marketing – Relazioni Internazionali – Sistemi Informativi Aziendali)."
         }
       ]
     },
@@ -78,14 +78,14 @@ var APP_DATA = {
         {
           "yaw": 2.547889259442984,
           "pitch": 0.023695641773834097,
-          "title": "Title",
-          "text": "Text"
+          "title": "Lettore Badge",
+          "text": " Il rilevatore di presenze, ideato e realizzato da alcuni studenti dell’Istituto, viene utilizzato per leggere il badge nominativo che ogni alunno porta sempre con sé, e che permette di gestire, in maniera informatizzata, il controllo quotidiano della frequenza scolastica."
         },
         {
           "yaw": 0.3203570437345977,
           "pitch": -0.006894116899500702,
-          "title": "Title",
-          "text": "Text"
+          "title": "Hall",
+          "text": "La hall dell’IT “L. Di Maggio” rappresenta il luogo di accoglienza per docenti, alunni e genitori. E’ da qui che prendono avvio i percorsi che conducono agli ambienti didattici dei diversi indirizzi di studio."
         }
       ]
     },
@@ -129,8 +129,8 @@ var APP_DATA = {
         {
           "yaw": -0.0024036270184844,
           "pitch": -0.03394990335053549,
-          "title": "ih",
-          "text": "Text"
+          "title": "Aula Magna",
+          "text": "L’Aula Magna è un auditorium capace di ospitare circa 200 persone. Dotato di impianti video ed audio, viene utilizzata per gli incontri collegiali scolastici, ma anche per manifestazioni e convegni aperti al pubblico."
         }
       ]
     },
@@ -158,8 +158,8 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
+        "yaw": 2.5918997128513457,
+        "pitch": 0.0011261786037124466,
         "fov": 1.3848081938437478
       },
       "linkHotspots": [
@@ -170,7 +170,14 @@ var APP_DATA = {
           "target": "1-hall"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.086911472944605,
+          "pitch": -0.022822488784779438,
+          "title": "Biblioteca",
+          "text": "La Biblioteca, oltre ad ospitare libri e volumi dei più diversi generi, dalla letteratura classica alla fantascienza, è attrezzata per svolgere attività didattica con piccoli gruppi."
+        }
+      ]
     },
     {
       "id": "4-aula",
@@ -212,8 +219,8 @@ var APP_DATA = {
         {
           "yaw": -2.78499201492615,
           "pitch": 0.1863280724769627,
-          "title": "ih",
-          "text": "Text"
+          "title": "Aula",
+          "text": "Le nostre aule, tutte dotate di LIM, computer ed accesso ad internet, sono ampie e luminose, così come previsto dalla normativa vigente che punta a garantire il benessere scolastico degli alunni."
         }
       ]
     },
@@ -257,8 +264,8 @@ var APP_DATA = {
         {
           "yaw": -0.23844009391408072,
           "pitch": -0.02271378406950575,
-          "title": "Title",
-          "text": "Text"
+          "title": "Laboratorio di Informatica",
+          "text": "Il Laboratorio d’Informatica, dotato di postazioni multimediali collegate in rete tramite fibra, è uno degli ambienti didattici utilizzati dagli alunni che frequentano l’indirizzo di Informatica e Telecomunicazioni. Gli studenti qui potranno mettere in pratica quanto appreso durante l’attività svolta in aula, esercitandosi nell’utilizzo degli specifici linguaggi di programmazione che consentono loro di creare programmi, applicazioni, app per smartphone e programmazione per il mondo web."
         }
       ]
     },
@@ -302,8 +309,8 @@ var APP_DATA = {
         {
           "yaw": -0.23736902482881206,
           "pitch": 0.039276412624804635,
-          "title": "Title",
-          "text": "Text"
+          "title": "Laboratorio di Sistemi e Reti",
+          "text": "Il laboratorio di Sistemi e Reti, dotato di postazioni multimediali collegate in rete tramite fibra, è un altro degli ambienti didattici utilizzati dagli alunni che frequentano il triennio dell’indirizzo informatico. Viene utilizzato per mettere in pratica le principali nozioni teoriche sullo studio dei sistemi digitali, e su come poter realizzare una rete informatica domestica o aziendale. Inoltre vengono illustrati i microcontrollori (Arduino) e single-board computer (Raspberry), i loro sensori, con applicazioni pratiche nel mondo della robotica."
         }
       ]
     },
@@ -347,8 +354,8 @@ var APP_DATA = {
         {
           "yaw": -0.8150373784937557,
           "pitch": 0.20658175787426813,
-          "title": "ih",
-          "text": "Text"
+          "title": "Laboratorio di Chimica Organica",
+          "text": "Il Laboratorio di Chimica Organica, recentemente riorganizzato ed opportunamente attrezzato, è utilizzato dagli alunni di Biotecnologie Sanitarie per eseguire le procedure di separazione e purificazione di composti organici (separazione estrattiva, cristallizzazione, cromatografia, distillazione), oltre che per sperimentare semplici reazioni di chimica organica riguardanti la reattività di gruppi funzionali."
         }
       ]
     },
@@ -392,8 +399,8 @@ var APP_DATA = {
         {
           "yaw": -1.663579284430611,
           "pitch": -0.07274514758769612,
-          "title": "INFOH",
-          "text": "Text"
+          "title": "Lab. Chimica Analitica",
+          "text": "Il Laboratorio di Chimica Analitica è utilizzato, oltre che dagli alunni del biennio, anche da quelli che frequentano il triennio di Biotecnologie Sanitarie, per eseguire analisi chimiche finalizzate a studiare la composizione della materia, sia da un punto di vista qualitativo che quantitativo."
         }
       ]
     },
@@ -437,8 +444,8 @@ var APP_DATA = {
         {
           "yaw": -0.38336365631528757,
           "pitch": 0.32749003307925406,
-          "title": "ih",
-          "text": "Text"
+          "title": "Lab. di Biologia e Microbiologia",
+          "text": "Nel Laboratorio di Biologia e Microbiologia, utilizzato dagli studenti del triennio di Biotecnologie Sanitarie, vengono condotti esperimenti per approfondire la struttura, le attività e i ruoli in natura di microrganismi quali: batteri, funghi, alghe e virus."
         }
       ]
     },
@@ -480,10 +487,10 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": -1.1127665655677283,
-          "pitch": 0.30069805848995124,
-          "title": "hotspot",
-          "text": "Text"
+          "yaw": -0.13985405126198813,
+          "pitch": -0.2618903052265029,
+          "title": "Laboratorio di Anatomia",
+          "text": "Il Laboratorio di Anatomia, la cui frequenza è riservata agli studenti del triennio di Biotecnologie Sanitarie, accoglie modelli di strutture del corpo umano e strumenti che servono per approfondire lo studio di organi, apparati e tessuti. Tra le esercitazioni pratiche che si svolgono in questo ambiente didattico, ci sono anche le sezioni di organi animali (occhio, cuore, cervello) utili ad approfondire la conoscenza della strutture anatomiche."
         }
       ]
     },
@@ -523,7 +530,14 @@ var APP_DATA = {
           "target": "1-hall"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.0022205743761709584,
+          "pitch": -0.019982491470001307,
+          "title": "Laboratorio di Fisica",
+          "text": "Nel Laboratorio di Fisica, utilizzato esclusivamente dagli alunni del biennio, c’è la possibilità di sperimentare praticamente quanto appreso durante le lezioni teoriche. E’ dotato di apparecchiature e strumenti per realizzare esperimenti di meccanica e termodinamica."
+        }
+      ]
     },
     {
       "id": "12-lab-elettrotecnica-1",
@@ -571,8 +585,8 @@ var APP_DATA = {
         {
           "yaw": -2.503138203240912,
           "pitch": -0.03870167232765098,
-          "title": "ih",
-          "text": "Text"
+          "title": "Laboratorio di Elettrotecnica",
+          "text": "Il Laboratorio di Elettrotecnica è utilizzato dagli studenti del triennio di Elettronica ed Elettrotecnica, per svolgere attività di Domotica, oltre che per apprendere le conoscenze tecniche fondamentali utili alla realizzazione di impianti ad energia rinnovabile con pannelli fotovoltaici e/o pale eoliche."
         }
       ]
     },
@@ -618,7 +632,14 @@ var APP_DATA = {
           "target": "12-lab-elettrotecnica-1"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 2.081049503399389,
+          "pitch": 0.2470694107495497,
+          "title": "Laboratorio di Elettrotecnica #2",
+          "text": "Il Laboratorio di Elettrotecnica è utilizzato dagli studenti del triennio di Elettronica ed Elettrotecnica, per mettere in pratica quanto studiato su come progettare, realizzare e gestire sistemi elettronici ed impianti, sia civili che industriali."
+        }
+      ]
     },
     {
       "id": "14-lab-disegno-cad",
@@ -660,8 +681,8 @@ var APP_DATA = {
         {
           "yaw": 0.7867213955436867,
           "pitch": 0.27081233093646695,
-          "title": "Title",
-          "text": "Text"
+          "title": "Laboratorio di T.T.R.G.",
+          "text": "Nel Laboratorio di Tecnologie e Tecniche di Rappresentazione Grafica, gli studenti del biennio imparano ad utilizzare programmi informatici di disegno, con l’ausilio di software CAD, per riprodurre al computer immagini di oggetti che saranno poi realizzati tridimensionalmente per mezzo di un’apposita stampante 3D."
         }
       ]
     },
@@ -701,7 +722,14 @@ var APP_DATA = {
           "target": "1-hall"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.20081233093646695,
+          "pitch": 0.00000033093646695,
+          "title": "Laboratorio Lingue e Tec. Informatiche",
+          "text": "Il Laboratorio di Lingue e di Tecnologie Informatiche, opportunamente attrezzato, viene utilizzato sia per far approfondire agli studenti la conoscenza delle lingue straniere, indispensabile per garantire la loro partecipazione ai diversi PON che si svolgono all’estero, e sia come ambiente didattico, dove gli studenti del primo anno approcciano le basi dell’informatica e acquisiscono le competenze digitali che ogni cittadino nel mondo moderno deve possedere."
+        }
+      ]
     },
     {
       "id": "16-palestra-grande",
@@ -739,7 +767,14 @@ var APP_DATA = {
           "target": "1-hall"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -0.009945717123452269,
+          "pitch": -0.1454865383595152,
+          "title": "Palestra Grande",
+          "text": "La Palestra dell’I.T. “L. Di Maggio” è una delle strutture scolastiche più grandi ed attrezzate di tutta la regione. Dotata di doppie tribune e pavimento in parquet, è ideale per permettere, non solo ai nostri alunni ma anche alle diverse associazioni presenti sul territorio, di svolgere molteplici attività e gare sportive, sia a livello dilettantistico che professionistico."
+        }
+      ]
     },
     {
       "id": "17-palestra-piccola",
@@ -777,7 +812,14 @@ var APP_DATA = {
           "target": "1-hall"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 3.0990270500428245,
+          "pitch": -0.15370976661452218,
+          "title": "Palestra Piccola",
+          "text": "La Palestra piccola, utilizzata dai nostri alunni durante le lezioni di Scienze Motorie, è attrezzata per svolgere attività fisica, oltre che allenamenti e gare, sia di pallavolo che di pallacanestro."
+        }
+      ]
     },
     {
       "id": "18-ite---lab-informatica",
@@ -808,7 +850,14 @@ var APP_DATA = {
         "fov": 1.3848081938437478
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -0.5484005124233153,
+          "pitch": -0.05196354160320382,
+          "title": "ITE - Laboratorio di Informatica",
+          "text": "Il Laboratorio d’Informatica, dotato di LIM e postazioni multimediali collegate in rete tramite fibra, è utilizzato dagli alunni che frequentano l’Istituto Tecnico Economico per svolgere attività didattiche che prevedano l’utilizzo di programmi di video scrittura e di fogli elettronici di calcolo, oltre che per approfondire la conoscenza dei sistemi informativi aziendali e acquisire le basi della programmazione software."
+        }
+      ]
     },
     {
       "id": "19-ite---lab-economia-aziendale",
@@ -834,12 +883,19 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -2.8117665592141243,
-        "pitch": 0.07032254921518444,
+        "yaw": 0.30009165974050234,
+        "pitch": 0.016684436070404374,
         "fov": 1.3848081938437478
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.25573111155751604,
+          "pitch": -0.00431648417003494,
+          "title": "ITE - Lab. Economia Aziendale",
+          "text": "Il Laboratorio di Economia Aziendale è dotato di LIM oltre che di postazioni multimediali collegate in rete tramite fibra. Qui gli studenti dei diversi indirizzi di studio possono mettere in pratica quanto appreso durante l’attività svolta in aula, esercitandosi nell’utilizzo di programmi specifici per la gestione economica ed aziendale."
+        }
+      ]
     }
   ],
   "name": "IT 'Luigi Di Maggio' - Tour Virtuale",
