9e6fd1c1c0b13f20cc610b3ab6326925f67a14d6
Vittorio Lo Mele <vittorio@mrbackslash.it>
2020-12-21 11:13:34 +0100

    tour su github + fix vari

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,3 +1,9 @@
+/*
+Autore:       Vittorio Lo Mele
+Data:         19-12-2020
+Descrizione:  Overlay manager (versione homepage)
+*/
+
 function overlayOn(){
     $("#overlayMain").css("display", "block");
 }
--- tour/overlaymanager.js
+++ tour/overlaymanager.js
@@ -1,3 +1,9 @@
+/*
+Autore:       Vittorio Lo Mele
+Data:         19-12-2020
+Descrizione:  Overlay manager (versione tour)
+*/
+
 function overlayOn(ytid){
     document.getElementById("overlayIframe").src = "https://www.youtube-nocookie.com/embed/" + ytid;
     document.getElementById("overlayMain").style.display = "block";
